07b71058abf95f5a97343d576ce90ec64904f771
virgilio macero <virgiliomacero@gmail.com>
2024-02-23 02:34:30 -0500

    Creating and giving some Fixes to the Header

---
+++ src/components/HeaderButton.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function HeaderButton({
+  title = "Title",
+  url = "",
+}: {
+  title: string;
+  url: string;
+}) {
+  return (
+    <a
+      href={url}
+      className="p-2 items-center text-center rounded-3xl  px-6 shadow-xl hover:border-2"
+    >
+      {title}
+    </a>
+  );
+}

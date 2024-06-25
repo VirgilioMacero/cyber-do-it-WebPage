9f0eb3711ccb9792dece701eddab18aa4f3e10f5
Andres Apostol <aandresaapostol@gmail.com>
2024-06-25 15:16:05 -0400

    refactorize linkButton

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -1,36 +1,22 @@
 export default function LinkButton({
   name = "Button",
   link = "",
-  bgColor = "#f0f0f0",
-  hoverBg = "#4361ED",
-  textColor = "#3D2DA2",
   className = "",
 }: {
   name?: string;
   link?: string;
-  bgColor?: string;
-  hoverBg?: string;
-  textColor?: string;
   className?: string;
 }) {
-  function MouseOver(event: any) {
-    event.target.style.background = hoverBg;
-  }
-  function MouseOut(event: any) {
-    event.target.style.background = bgColor;
-  }
+
 
   return (
     <a href={link}>
       <p
         style={{
-          backgroundColor: bgColor,
-          color: textColor,
           fontFamily: "Azeret Mono",
         }}
         className={`p-4 rounded-full ${className}`}
-        onMouseOver={MouseOver}
-        onMouseOut={MouseOut}
+
       >
         {name}
       </p>
--- src/components/NavProducts.tsx
+++ src/components/NavProducts.tsx
@@ -9,10 +9,12 @@ export default function NavProducts(){
                     <button className="bg-gradient-to-r from-green-400 to-blue-500 hover:from-green-500 hover:to-blue-600 text-white font-bold py-6 px-8 rounded-full shadow-md text-3xl">
                         SOCIAL MEDIA
                     </button>
+
                 <LinkButton 
                     className="bg-gradient-to-r from-green-400 to-blue-500 hover:from-green-500 hover:to-blue-600 text-white font-bold py-6 px-8 rounded-full shadow-md text-3xl"
                     name="DESING"
                     />
+
                     <button className="bg-gradient-to-r from-purple-400 to-pink-500 hover:from-purple-500 hover:to-pink-600 text-white font-bold py-2 px-4 rounded-full shadow-md">
                         DESIGN
                     </button>
